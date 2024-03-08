1f92a22e517b4205d7f7c34a85e232f2caf01c5a
Wasi <stwasihere@gmail.com>
2024-03-08 15:24:01 +0530

    Cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express();
 const dotenv = require('dotenv').config();
+const cors = require('cors')
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+app.user(cors());
 
 const {
     GoogleGenerativeAI,
